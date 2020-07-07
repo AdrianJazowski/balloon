f73c3971b3a570ee980cf0349efa7d930548a5a3
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-07-07 18:33:42 +0200

    Init projcet and implement Animeted component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Animated from "./Animated";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="balloon-animation">
+        <Animated
+          className="flying-cloud"
+          src="../assets/cloud.png"
+          ratioX="0.26"
+          ratioY="0.34"
+        />
+        <Animated
+          className="flying-cloud"
+          src="../assets/cloud.png"
+          ratioX="0.66"
+          ratioY="0.44"
+        />
+        <Animated
+          className="flying-cloud"
+          src="../assets/cloud.png"
+          ratioX="0.56"
+          ratioY="0.44"
+        />
+        <Animated
+          className="flying-cloud"
+          src="../assets/cloud.png"
+          ratioX="0.16"
+          ratioY="0.24"
+        />
+        <Animated
+          className="flying-cloud"
+          src="../assets/cloud.png"
+          ratioX="0.46"
+          ratioY="0.24"
+        />
+        <Animated
+          className="flying-balloon"
+          src="../assets/balloon.png"
+          ratioX="0.56"
+          ratioY="0.54"
+        />
+      </div>
     </div>
   );
 }
